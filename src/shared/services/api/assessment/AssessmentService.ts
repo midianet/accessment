ca59cbf60ea01d779f1a6b7183862de98b680f8f
Marcos Fernando <midianet@gmail.com>
2022-12-19 15:40:03 -0300

    alteraççoes diarias

--- src/shared/services/api/assessment/AssessmentService.ts
+++ src/shared/services/api/assessment/AssessmentService.ts
@@ -1,13 +1,13 @@
 import { Environment } from '../../../environment';
 import { Api } from '../axios-config';
 
-export interface AssessmentGrupo{
+export interface AssessmentGrupo {
   id: number;
   nome: string;
   elementos: Assessment[];
 }
 
-export interface Assessment{
+export interface Assessment {
     id?:number;
     maturidade?:string;
     perguntaId: number;
@@ -15,26 +15,53 @@ export interface Assessment{
     perguntaAjuda: string;
 }
 
+export interface AssessmentRadar {
+  grupo:string;
+  valor:number;
+}
+
 export interface AssessmentList  {
     data: AssessmentGrupo[];
 }
 
 const get = async (projeto:number, disciplina?:number): Promise<AssessmentList | Error> => {
-  try{
+  try {
     const { data } = await Api.get(`${Environment.ASSESSMENT_API}/${projeto}${disciplina ? `?disciplinaId=${disciplina}`: ''}`);
-    console.log(data);
     if (data) {
-      return {
-        data : data.grupos,
-      };
+      return { data : data.grupos};
     }
     return new Error(Environment.REGISTRO_LISTA_ERRO);
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     return new Error((error as {message: string}).message || Environment.REGISTRO_LISTA_ERRO);
   }
 };
 
+const radar = async (projeto:number): Promise<AssessmentRadar[] | Error> => {
+  try {
+    const { data } = await Api.get(`${Environment.ASSESSMENT_API}/${projeto}/radar`);
+    if (data) return data;
+    return new Error(Environment.REGISTRO_LISTA_ERRO);
+  } catch (error) {
+    console.error(error);
+    return new Error((error as {message: string}).message || Environment.REGISTRO_LISTA_ERRO);
+  }
+};
+
+
+const save = async (projetoId:number, assessment: Assessment): Promise<number | Error> => {
+  try{
+    const { data } = await Api.post<number>(`${Environment.RESPOSTA_API}/${projetoId}`, assessment);
+    if (data)  return data;
+    return new Error(Environment.REGISTRO_CRIAR_ERRO);
+  }catch(error) {
+    console.log(error);
+    return new Error((error as {message: string}).message || Environment.REGISTRO_CRIAR_ERRO);
+  }
+};
+
 export const AssessmentService = {
   get,
+  radar,
+  save
 };
